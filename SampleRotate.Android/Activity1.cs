ac00f92ce59d4705a8be8fcfb985742602bc1bd4
Tomoaki Masuda <masuda@moonmile.net>
2014-03-30 11:19:53 +0900

    init

---
+++ SampleRotate.Android/Activity1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SampleRotate.Core.Model;
+
+namespace SampleRotate.Android
+{
+    [Activity(Label = "SampleRotate.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        private DataModel _model;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // データ
+            _model = new DataModel
+            {
+                ID = "X20140330",
+                UserName = "Tomoaki",
+                Score = 100,
+                Rank = 10
+            };
+            UpdateData();
+        }
+
+        /// <summary>
+        /// 手動でバインド
+        /// </summary>
+        private void UpdateData()
+        {
+            FindViewById<TextView>(Resource.Id.textViewID).Text = _model.ID;
+            FindViewById<TextView>(Resource.Id.textViewUserName).Text = _model.UserName;
+            FindViewById<TextView>(Resource.Id.textViewScore).Text = _model.Score.ToString();
+            FindViewById<TextView>(Resource.Id.textViewRank).Text = _model.Rank.ToString();
+        }
+    }
+}
+
---
+++ SampleRotate.Core/Model/DataModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleRotate.Core.Model
+{
+    public class DataModel : BindableBase
+    {
+        private string _ID = "XYZ000";
+        public string ID
+        {
+            get { return _ID; }
+            set { this.SetProperty(ref this._ID, value); }
+        }
+        private string _UserName;
+        public string UserName
+        {
+            get { return _UserName; }
+            set { this.SetProperty(ref this._UserName, value); }
+        }
+        private int _Score;
+        public int Score
+        {
+            get { return _Score; }
+            set { this.SetProperty(ref this._Score, value); }
+        }
+        private int _Rank;
+        public int Rank
+        {
+            get { return _Rank; }
+            set { this.SetProperty(ref this._Rank, value); }
+        }
+    }
+}
